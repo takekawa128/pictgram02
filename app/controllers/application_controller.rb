9e7199e4bd6885d32ef9700c355bac8ac6fb67cf
takekawa128 <shion2002@live.jp>
2020-12-27 08:58:58 +0000

    ログイン機能の実装エラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   #訳：偽造からの保護、それは例外？
   protect_from_forgery with: :exception
-  
   add_flash_types :success, :info, :warning, :danger
+  
+  #現在ロインしているuserのこと
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #$ rails g controller Sessions newコマンドで下記ルーティングが作成された
+  get 'sessions/new'
   # get 'users/new'
   # get 'user/new'
   # #rails g contoroller pages indesxを入力したら、自動で下記が追加された。
@@ -10,5 +12,15 @@ Rails.application.routes.draw do
   get 'pages/help'
   # get 'pages/test'
   # Userのcreateアクションに対応するroutesも定義
-  resources :users
+  
+  
+  #Sessionの追加、下記３行
+  #get     '/login', to: 'sessions#new' はログインするためのフォームを表示するページを取得するだけなのでget（取得）
+  get     '/login', to: 'sessions#new'
+  #post    '/login', to: 'sessions#create'は新しくメールアドレスやパスワードを送信してもらい、データを元にSessionを作成するから、post（作成）
+  post    '/login', to: 'sessions#create'
+  #delete  'logout', to: 'sessions#destory'はログアウトでSessionを削除するからdelete（削除）
+  delete  'logout', to: 'sessions#destory'
+  
+  resources :users  
 end
